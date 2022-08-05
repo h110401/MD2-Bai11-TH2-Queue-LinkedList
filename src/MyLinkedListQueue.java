9fe336e59aaa176bb62e3289b3256279bc31cb70
h110401 <hung110401@gmail.com>
2022-08-05 19:52:46 +0700

    th2

---
+++ src/MyLinkedListQueue.java
@@ -0,0 +1,46 @@
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+
+    public MyLinkedListQueue() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void enqueue(int key) {
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+
+    public Node dequeue() {
+        if (this.head == null) return null;
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null) this.tail = null;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(4);
+        queue.dequeue();
+        queue.enqueue(56);
+        queue.enqueue(2);
+        queue.enqueue(67);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(24);
+        queue.dequeue();
+        queue.enqueue(98);
+        queue.enqueue(45);
+        queue.enqueue(23);
+        queue.enqueue(435);
+
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    int key;
+    Node next;
+
+    public Node(int key) {
+        this.key = key;
+    }
+}
